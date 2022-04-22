ec8ac6d7c02dc813a86854de8466eb4ac9631491
talsim <talsimhayev@gmail.com>
2022-04-22 13:57:08 +0300

    Fixed the bug where there was dangling OnClickListeners on the board

--- Checkers/app/src/main/java/com/example/checkers/MyOnClickListenerForPieceMoves.java
+++ Checkers/app/src/main/java/com/example/checkers/MyOnClickListenerForPieceMoves.java
@@ -7,16 +7,14 @@ import android.widget.ImageView;
 public class MyOnClickListenerForPieceMoves implements View.OnClickListener {
 
     public static final String TAG = "MyListenerForPieceMoves";
-    // private static ImageView[] lastUsedImageViews; // for removing the setOnClickListeners that we set and the player did not choose, so there will not be hanging listeners.
+    private static ImageView[] lastUsedImageViews; // for removing the setOnClickListeners that we set and the player did not choose, so there will not be hanging listeners.
     private final Piece piece;
     private final Board board;
 
     public MyOnClickListenerForPieceMoves(Piece piece, Board board) {
         this.piece = piece;
         this.board = board;
-//        lastUsedImageViews = new ImageView[4]; //  ################### CHANGE THE SIZE OF THE ARRAY ACCORDING TO THE AMOUNT OF setOnClickListeners YOU HAVE!!!!!!!!!!!#############
-//        lastUsedImageViews[0] = null;
-//        lastUsedImageViews[1] = null;
+        lastUsedImageViews = new ImageView[4]; //  ################### CHANGE THE SIZE OF THE ARRAY ACCORDING TO THE AMOUNT OF setOnClickListeners YOU HAVE!!!!!!!!!!!#############
     }
 
     @Override
@@ -27,7 +25,7 @@ public class MyOnClickListenerForPieceMoves implements View.OnClickListener {
     private void displayMoveOptionsAndMove(int x, int y, boolean isBlack) {
 
         clearPossibleLocationMarkers();
-        //unsetOnClickLastImageViews();
+        unsetOnClickLastImageViews();
 
 
         // for black
@@ -36,7 +34,7 @@ public class MyOnClickListenerForPieceMoves implements View.OnClickListener {
             if (Logic.canBlackMove(x) && !Logic.isOnLeftEdge(y) && Logic.isTileAvailable(board, x - 1, y - 1) /* left tile */) {
                 Move leftMove = new Move(x, y, x - 1, y - 1);
                 ImageView leftPieceImage = StartGameActivity.imageViewsTiles[x - 1][y - 1];
-                //lastUsedImageViews[0] = leftPieceImage;
+                lastUsedImageViews[0] = leftPieceImage;
                 leftPieceImage.setImageResource(R.drawable.possible_location_marker);
                 leftPieceImage.setClickable(true);
                 leftPieceImage.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +48,7 @@ public class MyOnClickListenerForPieceMoves implements View.OnClickListener {
                         board.getBoardArray()[endX][endY] = new Piece(leftPieceImage, endX, endY, true);
                         board.getBoardArray()[x][y] = null; // remove old piece
                         clearPossibleLocationMarkers();
-                        //unsetOnClickLastImageViews();
+                        unsetOnClickLastImageViews();
 
                         // set onClick for the new piece (location)
                         leftPieceImage.setClickable(true);
@@ -68,7 +66,7 @@ public class MyOnClickListenerForPieceMoves implements View.OnClickListener {
             if (Logic.canBlackMove(x) && !Logic.isOnRightEdge(y) && Logic.isTileAvailable(board, x - 1, y + 1) /* right tile */) {
                 Move rightMove = new Move(x, y, x - 1, y + 1);
                 ImageView rightPieceImage = StartGameActivity.imageViewsTiles[x - 1][y + 1];
-                //lastUsedImageViews[1] = rightPieceImage;
+                lastUsedImageViews[1] = rightPieceImage;
                 rightPieceImage.setImageResource(R.drawable.possible_location_marker);
                 rightPieceImage.setClickable(true);
                 rightPieceImage.setOnClickListener(new View.OnClickListener() {
@@ -82,7 +80,7 @@ public class MyOnClickListenerForPieceMoves implements View.OnClickListener {
                         board.getBoardArray()[endX][endY] = new Piece(rightPieceImage, endX, endY, true);
                         board.getBoardArray()[x][y] = null; // remove old piece
                         clearPossibleLocationMarkers();
-                        //unsetOnClickLastImageViews();
+                        unsetOnClickLastImageViews();
 
                         // set onClick for the new piece (location)
                         rightPieceImage.setClickable(true);
@@ -107,20 +105,29 @@ public class MyOnClickListenerForPieceMoves implements View.OnClickListener {
         }
     }
 
-//    public void unsetOnClickLastImageViews() {
-//        for (ImageView image : lastUsedImageViews) {
-//            int x = piece.getX();
-//            int y = piece.getY();
-//            if (image != null) {
-//                if (!Logic.isOnLeftEdge(y) && Logic.isTileAvailable(board, x - 1, y - 1)) {
-//                    if (!Logic.isOnRightEdge(y) && Logic.isTileAvailable(board, x - 1, y + 1)) {
-//                        image.setClickable(false);
-//                        image.setOnClickListener(null);
-//                    }
-//                }
-//            }
-//        }
-//
-//    }
+    public void unsetOnClickLastImageViews() {
+        // how to make sure that at the place of the image there isn't also a checkers piece:
+        // 1. get id of image; extract X and Y axis from it
+        // 2. compare those X and Y in boardArray
+        // 3. if a piece is found at that location: do not unset it!
+        // 4. else: unset it.
+        for (ImageView image : lastUsedImageViews) {
+            if (image != null) {
+                // get id of image and extract axis
+                String idStr = image.getResources().getResourceEntryName(image.getId());
+                String XAndY = idStr.substring(idStr.length() - 2);
+                int x = Character.getNumericValue(XAndY.charAt(0));
+                int y = Character.getNumericValue(XAndY.charAt(1));
+
+
+                if (board.getBoardArray()[x][y] == null) {
+                    image.setClickable(false);
+                    image.setOnClickListener(null);
+                }
+            }
+        }
+
+
+    }
 }
 
